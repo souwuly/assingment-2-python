96075de6835bfda7a6f09c843acb788d90b4f742
souwuly <98064085+souwuly@users.noreply.github.com>
2022-09-27 03:17:01 +0600

    Add files via upload

---
+++ tasks.py
@@ -0,0 +1,19 @@
+import os
+
+# 1 task
+#os.mkdir("folder") - creating new directory
+#os.rmdir("folder") - deleting the directory
+
+# os.mkdir("hmm")
+# os. chdir("hmm") - changing/updating the directory
+#os. listdir("hmm") - read the directory
+
+# 2 task
+# a = 0
+# n = int(input("Please enter the number of integers"))
+# for l in range(n):
+#   ele = int(input())
+#   a = a + ele
+# print (a)  
+
+# 3 task
