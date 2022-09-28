35b1b8639c4d6145321ebd27d661072599c624dd
souwuly <98064085+souwuly@users.noreply.github.com>
2022-09-28 16:46:25 +0600

    Add files via upload

--- tasks.py
+++ tasks.py
@@ -1,4 +1,5 @@
 import os
+from re import X
 
 # 1 task
 #os.mkdir("folder") - creating new directory
@@ -17,3 +18,39 @@ import os
 # print (a)  
 
 # 3 task
+# def f1(func):
+#     def s_f():
+#         print("Start")
+#         func()
+#         print("finish")
+#     return s_f    
+
+# @f1
+# def check(): 
+#     n = '29'
+#     print(type(n))
+#     conv_n = int(n)
+#     print(type(conv_n)) 
+# check()
+
+# 4 task
+
+# class Main:
+#     def __init__(self, l, w):
+#          self.length=l
+#          self.width=w
+
+#     def Area(self):
+#          s = (self.length * self.width)
+#          print("S = ",s)         
+        
+#     def Perimeter(self):
+#          p = (self.length + self.width)*2
+#          print("P = ",p)
+
+# class Subm(Main):
+#      def __init__(self, l, w):
+#          super().__init__(l, w)
+
+# recTA = Subm(45,29).Area()
+# recTP = Subm(45,29).Perimeter()
